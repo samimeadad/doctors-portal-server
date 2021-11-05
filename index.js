f570f2a07d22c6801d453e04e2302c34e4086681
Sami Meadad Choudhury <sami.meadad@gmail.com>
2021-11-05 21:52:44 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require( 'express' );
+const app = express();
+const cors = require( 'cors' );
+require( 'dotenv' ).config()
+const port = process.env.PORT || 5000;
+const { MongoClient } = require( 'mongodb' );
+
+//middleware
+app.use( cors() );
+app.use( express.json() );
+
+const uri = `mongodb+srv://${ process.env.DB_USER }:${ process.env.DB_PASS }@cluster0.iezc6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient( uri, { useNewUrlParser: true, useUnifiedTopology: true } );
+
+const run = async () => {
+    try {
+        await client.connect();
+        console.log( 'Connected to MongoDB Database' );
+
+    } catch ( err ) {
+        console.error( err );
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch( console.dir );
+
+app.get( '/', ( req, res ) => {
+    res.send( 'Hello Doctors Portal!' )
+} )
+
+app.listen( port, () => {
+    console.log( `Doctors Portal App Server listening at http://localhost:${ port }` )
+} )
